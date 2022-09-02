6eaf6e0ade70bdce114be2b53b56a6e0319ce3f0
Yasin5050 <99257467+Yasin5050@users.noreply.github.com>
2022-09-02 12:39:01 +0300

    Add files via upload

---
+++ v13-ticket-main/events/ready.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+
+    async execute(client) {
+        console.log(`Giriş Başarılı!`)
+client.user.setActivity("Ticket <3 Ly3ssia")
+      
+    }
+}
